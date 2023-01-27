901c0df4389dc70bbc333fa0420c3cebed004e9e
“valdidev” <iamvaldidev@gmail.com>
2023-01-27 12:34:18 +0100

    feat: random turn works

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 function Board() {
-  return <div>This a is the board</div>;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
+    Math.round(Math.random() * 1) === 1 ? "X" : "O"
+  );
+  return (
+    <div>
+      <p>Ey! {currentPlayer}, your turn</p>
+      This a is the board
+    </div>
+  );
 }
 
 export default Board;
